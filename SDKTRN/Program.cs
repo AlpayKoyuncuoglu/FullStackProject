1bb2a5f7ee3ec033a5536ee258311f05887f3e42
AlpayKoyuncuoglu <alpaykoyuncuoglu1@gmail.com>
2023-02-25 21:19:27 +0300

    4

--- SDKTRN/Program.cs
+++ SDKTRN/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SDKTRN.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SocialContext>(x => x.UseSqlServer("Data Source=DESKTOP-61LAKB4;Initial Catalog=FullStck;Integrated Security=True"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
