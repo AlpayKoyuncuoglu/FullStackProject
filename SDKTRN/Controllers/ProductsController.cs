85309e7fcb8d8be1139ec7ac9842597b2622a7d0
AlpayKoyuncuoglu <alpaykoyuncuoglu1@gmail.com>
2023-02-25 21:34:19 +0300

    5

--- SDKTRN/Controllers/ProductsController.cs
+++ SDKTRN/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SDKTRN.Data;
 using SDKTRN.Models;
 
 namespace SDKTRN.Controllers
@@ -15,28 +17,30 @@ namespace SDKTRN.Controllers
         //    "samsung s8",
         //};
 
-        private static List<Product> _products;
-
-        public ProductsController()
+        //private static List<Product> _products;
+        private readonly SocialContext _context;
+        public ProductsController(SocialContext context)
         {
-            _products = new List<Product>();
-            _products.Add(new Product() { ProductId = 1, Name = "Samsung 6", Price = 3000, IsActive = false });
-            _products.Add(new Product() { ProductId = 2, Name = "Samsung 7", Price = 4000, IsActive = true });
-            _products.Add(new Product() { ProductId = 3, Name = "Samsung 8", Price = 5000, IsActive = true });
-            _products.Add(new Product() { ProductId = 4, Name = "Samsung 9", Price = 6000, IsActive = false });
-            _products.Add(new Product() { ProductId = 5, Name = "Samsung 10", Price = 7000, IsActive = true });
+            _context = context;
+            //_products = new List<Product>();
+            //_products.Add(new Product() { ProductId = 1, Name = "Samsung 6", Price = 3000, IsActive = false });
+            //_products.Add(new Product() { ProductId = 2, Name = "Samsung 7", Price = 4000, IsActive = true });
+            //_products.Add(new Product() { ProductId = 3, Name = "Samsung 8", Price = 5000, IsActive = true });
+            //_products.Add(new Product() { ProductId = 4, Name = "Samsung 9", Price = 6000, IsActive = false });
+            //_products.Add(new Product() { ProductId = 5, Name = "Samsung 10", Price = 7000, IsActive = true });
         }
 
         [HttpGet]
-        public List<Product> GetProducts()
+        public async Task<ActionResult> GetProducts()
         {
-            return _products;
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetProduct(int id)
+        public async Task<IActionResult> GetProduct(int id)
         {
-            var p = _products.FirstOrDefault(i => i.ProductId == id);
+            var p = await _context.Products.FirstOrDefaultAsync(i => i.ProductId == id);
             if (p == null)
 
             {
@@ -46,15 +50,15 @@ namespace SDKTRN.Controllers
                 return Ok(p);
         }
 
-        [HttpPost]
-        public IActionResult CreateProduct(Product p)
-        {
-            _products.Add(p);
-            foreach (var item in _products)
-            {
-                Console.WriteLine(item.Name);
-            }
-            return Ok(p);
-        }
+        //[HttpPost]
+        //public IActionResult CreateProduct(Product p)
+        //{
+        //    _products.Add(p);
+        //    foreach (var item in _products)
+        //    {
+        //        Console.WriteLine(item.Name);
+        //    }
+        //    return Ok(p);
+        //}
     }
 }
